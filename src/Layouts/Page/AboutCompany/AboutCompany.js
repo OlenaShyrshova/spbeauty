91b01d42f5c090d12cb318a05593ed9697b566a4
OlenaShyrshova <120008043+OlenaShyrshova@users.noreply.github.com>
2022-12-16 19:47:20 +0000

    come back

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -5,7 +5,7 @@ function AboutCompany() {
     return (
         <div>
             <Heading/> {/* Заголовок и кнопка справа */}
-            <div className='d-flex row'>
+            <div className='d-flex flex-column'>
                 <General/>  {/* Основная информация вроде названия, типа и телефона */}
                 <Addresses/> {/* Список доступных адресов заведений */}
             </div>
